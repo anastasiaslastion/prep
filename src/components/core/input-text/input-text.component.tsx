c87fa944c16f7bab7d7917adf5087899d3159b39
Anastasiia <anastasiia.slastion@gmail.com>
2023-02-21 16:06:07 +1100

    Input styles

--- src/components/core/input-text/input-text.component.tsx
+++ src/components/core/input-text/input-text.component.tsx
@@ -1,10 +1,15 @@
 import styles from './input-text.module.scss';
 
+import Icon from '../icon/icon.component';
+
 const InputText = () => {
     return (
-        <>
+        <div className={ styles.wrapper }>
+            <span className={ styles.icon }>
+                <Icon name='search' viewbox='0 96 960 960' />
+            </span>
             <input type='text' placeholder='Search...' className={ styles.input }/>
-        </>
+        </div>
     )
 }
 
